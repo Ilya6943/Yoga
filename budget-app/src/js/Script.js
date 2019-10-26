32e51bfec8994739a453fc9a1f3c62aebba297af
Ilya <kiv-123@mail.ru>
2019-10-26 20:01:18 +0300

    Six_Lesson

---
+++ budget-app/src/js/Script.js
@@ -0,0 +1,23 @@
+let btn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value'),
+    dayBudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value'),
+    input = document.getElementsByClassName('expenses-item'),
+    buttons = document.getElementsByTagName('button'),
+    btn1 = buttons[0],
+    btn2 = buttons[2],
+    input2 = document.querySelectorAll('.optionalexpenses-item'),
+    input3 = document.querySelector('.choose-income'),
+    input4 = document.getElementById('savings'),
+    input5 = document.querySelector('.choose-sum'),
+    input6 = document.querySelector('.choose-percent'),
+    input7 = document.querySelector('.year-value'),
+    input8 = document.querySelector('.month-value'),
+    input9 = document.querySelector('.day-value');
+
+console.log(input4);
